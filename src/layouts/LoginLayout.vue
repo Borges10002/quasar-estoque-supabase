ecec9f0b91f75c7eb39253d7109eb6ff5598eaab
borges10002 <borges10002@gmail.com>
2022-04-25 20:12:18 -0400

    Fist

---
+++ src/layouts/LoginLayout.vue
@@ -0,0 +1,25 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-toolbar-title> Stock App </q-toolbar-title>
+      </q-toolbar>
+    </q-header>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'LoginLayout',
+
+  setup () {
+    return {}
+  }
+})
+</script>
---
+++ src/pages/Me.vue
@@ -0,0 +1,24 @@
+<template>
+  <q-page class="flex flex-center">
+     <div v-if="user">
+      <div><h5 style="color:blue">Hello {{ user.user_metadata.name }} Happy birthday</h5> </div>
+    </div>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+import useAuthUser from 'src/composables/UseAuthUser'
+
+export default defineComponent({
+  name: 'PageMe',
+
+  setup () {
+    const { user } = useAuthUser()
+    return {
+      user
+    }
+  }
+})
+
+</script>

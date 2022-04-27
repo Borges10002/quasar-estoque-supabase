b18f13afe773d377bf3161f0c5776375124ff628
borges10002 <borges10002@gmail.com>
2022-04-26 20:42:27 -0400

    atualicao video 9

--- src/router/routes.js
+++ src/router/routes.js
@@ -10,13 +10,15 @@ const routes = [
       { path: 'email-confirmation', name: 'email-confirmation', component: () => import('pages/EmailConfirmation.vue') },
       { path: 'forgot-password', name: 'forgot-password', component: () => import('pages/ForgotPassword.vue') },
       { path: 'reset-password', name: 'reset-password', component: () => import('pages/ResetPassword.vue') }
+
     ]
   },
   {
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
     children: [
-      { path: 'me', name: 'me', component: () => import('pages/Me.vue') }
+      { path: 'me', name: 'me', component: () => import('pages/Me.vue') },
+      { path: 'category', name: 'category', component: () => import('pages/category/List.vue') }
     ],
     meta: {
       requiresAuth: true

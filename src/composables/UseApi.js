a76f074d6e71e4fca5448ad114116c4122045c17
borges10002 <borges10002@gmail.com>
2022-05-24 21:59:30 -0400

    video-28

--- src/composables/UseApi.js
+++ src/composables/UseApi.js
@@ -40,6 +40,19 @@ export default function useApi () {
     return data
   }
 
+  const fetchCount = async (table, userId) => {
+    const { data, error, count } =
+    await supabase
+      .from(table)
+      .select('*', { count: 'exact' })
+      .eq('user_id', userId)
+    if (error) throw error
+    return {
+      data,
+      count
+    }
+  }
+
   const getById = async (table, id) => {
     const { data, error } = await supabase
       .from(table)
@@ -125,6 +138,7 @@ export default function useApi () {
   return {
     list,
     listPublic,
+    fetchCount,
     getById,
     post,
     update,
